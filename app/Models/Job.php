eb660db46a227b4acca1e480cedcf9b6bcf79ca1
schuhmann <eberhard.schuhmann@gmail.com>
2020-12-04 11:20:56 +0100

    Initial commit

---
+++ app/Models/Job.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    public function tags ()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
