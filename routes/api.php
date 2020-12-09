7298864aacc3e4194084d4368867c553cab41bac
schuhmann <eberhard.schuhmann@gmail.com>
2020-12-09 15:49:32 +0100

    Schritt 13

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/user', LoginController::class);
-Route::post('/user', LogoutController::class);
+Route::post('/login', LoginController::class);
+Route::post('/logout', LogoutController::class);
